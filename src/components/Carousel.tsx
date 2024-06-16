4c2a04bcd21ae8e2f2339ee89f8625f7d88e12e6
Luiz Felipe <morfelle.luiz@gmail.com>
2024-06-16 11:18:13 -0300

    correcao barra lateral

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,12 +9,12 @@ const Carousel: React.FC = () => {
           <img
             src="/bg.jpeg"
             alt="Projeto 1"
-            className=" w-full object-cover filter brightness-90 saturate-200"
+            className=" w-full object-cover filter brightness-95 saturate-200"
           />
           <div>
             <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-zinc-900">
               <div className="absolute top-5 left-5 md:left-20 md:top-20 md:w-auto w-full h-auto">
-                <div className="flex flex-col justify-center items-start  h-full text-white text-opacity-90 text-2xl md:text-5xl font-bold leading italic">
+                <div className="flex flex-col justify-center items-start  h-full text-white text-opacity-90 text-2xl md:text-4xl font-bold leading italic">
                   <h2>CONSTRUINDO O</h2>
                   <h2 className="text-orange-700 pb-8">FUTURO</h2>
                   <h2>NIVELANDO O</h2>
